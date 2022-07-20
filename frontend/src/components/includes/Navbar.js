ebda45723858e441269b3fb85bd503cf0a6123ff
ktkdgh <ktkdgh821@gmail.com>
2022-07-20 22:14:11 +0900

    mergeing

--- frontend/src/components/includes/Navbar.js
+++ frontend/src/components/includes/Navbar.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Outlet } from "react-router"
-
+import LoginCheckModal from '../modal/LoginCheckModal';
 
 function Navbar() {
     const [ isLoggedIn, setIsLoggedIn ] = useState(false)
+    const [openModal, setOpenModal] = useState(false);
     const [ isName, setIsName] = useState("")
     
     useEffect(() => {
-        if (sessionStorage.getItem('userName')=== undefined || sessionStorage.getItem('userName') === null) return;
+        if (sessionStorage.getItem('userName') === undefined || sessionStorage.getItem('userName') === null) return;
         async function test() {
             if(sessionStorage.getItem('userName') !== null){
                 setIsLoggedIn(true)
@@ -31,8 +32,18 @@ function Navbar() {
                     <div className="navigation-bar"> 
                         <Link to="/" style={{textDecoration: 'none'}}><div className="navigation-bar-logo"> TECHTERVIEW </div></Link> 
                         <div className="navigation-bar-right">
-                            <Link to="/questionlist/main"><button className="interview-question-page-btn">질문 리스트</button></Link>
-                            <Link to="/feedback/main"><button className="interview-feedback-page-btn">피드백</button></Link>
+                            { isLoggedIn ? 
+                            <div>
+                                <Link to="/feedback/main"><button className="interview-feedback-page-btn">피드백</button></Link>
+                                <Link to="/questionlist/main"><button className="interview-question-page-btn">질문 리스트</button></Link>
+                            </div>
+                            :
+                            <div>
+                                <button className='interview-feedback-page-btn' onClick={() => { setOpenModal(true); }}  >피드백</button>
+                                <button className='interview-question-page-btn' onClick={() => { setOpenModal(true); }}  >질문 리스트</button>
+                                {openModal && <LoginCheckModal closeModal={setOpenModal} />}
+                            </div>
+                            }
                             { isLoggedIn ? 
                                 <div>
                                     <Link to="/feedback/myvideo"><button className="interview-my-page-btn">{ isName } 님</button></Link>
