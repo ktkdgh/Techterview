ed1bd4cd127106e2874b4e9dd8a40962150387a4
MiJung Lee <mijunglee@MiJungs-MacBook-Air.local>
2022-08-03 15:20:52 +0900

    edit modal style

--- frontend/src/components/modal/ReplyDeleteModal.js
+++ frontend/src/components/modal/ReplyDeleteModal.js
@@ -1,4 +1,5 @@
 import api from "../shared/api";
+import '../css/ReplyDeleteModal.css';
 
 function ReplyDeleteModal({ closeModal, replyId }) {
 
@@ -10,14 +11,14 @@ function ReplyDeleteModal({ closeModal, replyId }) {
     }
 
     return (
-        <div className="video-delete-modal">
-            <div className="video-delete-modal-content">
-                <div className="video-delete-modal-body">
+        <div className="reply-delete-modal">
+            <div className="reply-delete-modal-content">
+                <div className="reply-delete-modal-body">
                     댓글을 삭제하시겠습니까?
                 </div>
-                <div className="video-delete-modal-footer">
-                    <button className="btn-yes" onClick={() => closeModal(false)}>취소</button>
-                    <button className="btn-cancel" onClick={() => replyDelete(replyId)}>삭제</button>
+                <div className="reply-delete-modal-footer">
+                    <button className="reply-btn-cancel" onClick={() => closeModal(false)}>취소</button>
+                    <button className="reply-btn-delete" onClick={() => replyDelete(replyId)}>삭제</button>
                 </div>
             </div>
         </div>
