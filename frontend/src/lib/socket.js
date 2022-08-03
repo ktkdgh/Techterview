0812b3553710d1092a46e51dcc924d9e324879cf
ktkdgh <ktkdgh821@gmail.com>
2022-08-04 03:08:15 +0900

    master push push

--- frontend/src/lib/socket.js
+++ frontend/src/lib/socket.js
@@ -1,12 +1,7 @@
 import { io } from 'socket.io-client';
 
-// export let socket = io(process.env.REACT_APP_BASE_URL, {
-//   rejectUnauthorized: false // WARN: please do not do this in production
-// });
+//로콜용:
 // export let socket = io.connect('http://localhost:8000',{ perMessageDeflate: false }, {secure: true})
 
-//로콜용:
-export let socket = io.connect('http://localhost:8000',{ perMessageDeflate: false }, {secure: true})
-
 //배포용:
-// export let socket = io.connect('https://techterview.site', { perMessageDeflate: false }, { secure: true })
+export let socket = io.connect('https://techterview.site', { perMessageDeflate: false }, { secure: true })
